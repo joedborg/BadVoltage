2c4bd2f07709d5a7b55fbe252d43f2c2103b8df3
Joe Borg <cyborg101010@gmail.com>
2014-03-09 16:56:30 +0000

    Tidy up

--- listen/src/main/java/org/josephb/badvoltage/listen/Stream.java
+++ listen/src/main/java/org/josephb/badvoltage/listen/Stream.java
@@ -29,10 +29,7 @@ public class Stream extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
 }
